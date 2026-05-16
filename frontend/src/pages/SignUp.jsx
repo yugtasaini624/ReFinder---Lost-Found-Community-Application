5f2f8993468b708c2fd3f7d0baea1bb6badee51c
Yugta Saini <yugtasaini@gmail.com>
2026-05-16 17:23:44 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -36,7 +36,7 @@ const SignUp = () => {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/signup", {
+      const res = await fetch("https://refinder-backend.onrender.com/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
