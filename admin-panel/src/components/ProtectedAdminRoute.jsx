e7fc580b1ba36afc0d6d320b620105670b2d2393
Yugta Saini <yugtasaini@gmail.com>
2026-05-16 16:23:59 +0530

    Update API endpoint for admin verification

--- admin-panel/src/components/ProtectedAdminRoute.jsx
+++ admin-panel/src/components/ProtectedAdminRoute.jsx
@@ -13,7 +13,7 @@ const ProtectedAdminRoute = ({ children }) => {
       return;
     }
 
-    fetch("http://127.0.0.1:5000/api/verify-admin", {
+    fetch("https://refinder-backend.onrender.com/api/verify-admin", {
       headers: {
         Authorization: `Bearer ${token}`
       }
