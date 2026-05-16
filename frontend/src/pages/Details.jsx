47ee4ed017fcd242e5fc507ad7ba32a106204d80
Yugta Saini <yugtasaini@gmail.com>
2026-05-16 17:13:22 +0530

    Update Details.jsx

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -10,7 +10,7 @@ const Details = () => {
   const [showContact, setShowContact] = useState(false);
 
   useEffect(() => {
-    fetch(`http://127.0.0.1:5000/api/public/lost/${id}`)
+    fetch(`https://refinder-backend.onrender.com/api/public/lost/${id}`)
       .then(res => res.json())
       .then(data => setItem(data));
   }, [id]);
@@ -27,7 +27,7 @@ const Details = () => {
       <div className="fd-card">
 
         <img
-          src={`http://127.0.0.1:5000/uploads/${item.image}`}
+          src={`https://refinder-backend.onrender.com/uploads/${item.image}`}
           alt={item.item_name}
           className="fd-img"
         />
